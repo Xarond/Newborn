590a905119f570099f3c93617d02cdbfbe726353
Xarond <joszko.jakub@gmail.com>
2024-06-15 15:09:42 +0200

    ClassName and StructName

--- source/core/NewbornConfig.hpp
+++ source/core/NewbornConfig.hpp
@@ -11,4 +11,13 @@ using std::swap;
 using std::move;
 
 using std::unique_ptr;
+
+#define NEWBORN_CLASS(ClassName)
+    class Classname;
+    using ClassNameUPtr = std::unique_ptr<ClassName>;
+
+
+#define NEWBORN_STRUCT(StructName)
+    struct StructName;
+    using StructNameUPtr = std::unique_ptr<StructName>;
 }
