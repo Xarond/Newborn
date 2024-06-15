fbbf9a48ef5653eff64f1de6a89c252a5b01f2b5
Xarond <joszko.jakub@gmail.com>
2024-06-15 15:55:30 +0200

    Update Classname and structs

--- source/core/NewbornConfig.hpp
+++ source/core/NewbornConfig.hpp
@@ -11,13 +11,30 @@ using std::swap;
 using std::move;
 
 using std::unique_ptr;
+using std::shared_ptr;
+using std::weak_ptr;
+using std::make_shared;
+using std::make_unique;
+using std::static_pointer_cast;
+using std::dynamic_pointer_cast;
+using std::const_pointer_cast;
+using std::enable_shared_from_this;
 
-#define NEWBORN_CLASS(ClassName)
-    class Classname;
-    using ClassName##ConstUPtr = std::unique_ptr<const ClassName>
+#define STAR_CLASS(ClassName)                                     \
+  class ClassName;                                                \
+  using ClassName##Ptr = std::shared_ptr<ClassName>;              \
+  using ClassName##ConstPtr = std::shared_ptr<const ClassName>;   \
+  using ClassName##WeakPtr = std::weak_ptr<ClassName>;            \
+  using ClassName##ConstWeakPtr = std::weak_ptr<const ClassName>; \
+  using ClassName##UPtr = std::unique_ptr<ClassName>;             \
+  using ClassName##ConstUPtr = std::unique_ptr<const ClassName>
 
-
-#define NEWBORN_STRUCT(StructName)
-    struct StructName;
-    using StructName##ConstUPtr = std::unique_ptr<const StructName>
+#define STAR_STRUCT(StructName)                                     \
+  struct StructName;                                                \
+  using StructName##Ptr = std::shared_ptr<StructName>;              \
+  using StructName##ConstPtr = std::shared_ptr<const StructName>;   \
+  using StructName##WeakPtr = std::weak_ptr<StructName>;            \
+  using StructName##ConstWeakPtr = std::weak_ptr<const StructName>; \
+  using StructName##UPtr = std::unique_ptr<StructName>;             \
+  using StructName##ConstUPtr = std::unique_ptr<const StructName>
 }
