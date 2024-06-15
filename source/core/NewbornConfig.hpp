2ba7e9ae70a36a7d30928651d9b89621f796b1b9
Xarond <joszko.jakub@gmail.com>
2024-06-15 15:06:11 +0200

    starting with config

---
+++ source/core/NewbornConfig.hpp
@@ -0,0 +1,14 @@
+#pragma once
+//will create this shit later :<
+#include "NewbornPch.hpp"
+
+namespace Newborn {
+
+// Some common std namespaces includes
+using std::size_t;
+
+using std::swap;
+using std::move;
+
+using std::unique_ptr;
+}
