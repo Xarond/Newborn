045b15649b1f3d6a8e6d13975f83e06d74b4de80
Xarond <joszko.jakub@gmail.com>
2024-07-18 14:22:19 +0200

    more core tests
    added more core tests

---
+++ source/test/file_test.cpp
@@ -0,0 +1,51 @@
+#include "NewbornFile.hpp"
+#include "NewbornString.hpp"
+#include "NewbornFormat.hpp"
+
+#include "gtest/gtest.h"
+
+using namespace Newborn;
+
+TEST(FileTest, All) {
+  auto file = File::ephemeralFile();
+  file->resize(1000);
+  file->resize(0);
+  file->resize(500);
+  EXPECT_EQ(file->size(), 500);
+
+  auto dir = File::temporaryDirectory();
+  File::makeDirectory(File::relativeTo(dir, "inner"));
+  EXPECT_TRUE(File::isDirectory(File::relativeTo(dir, "inner") + "/"));
+  File::removeDirectoryRecursive(dir);
+
+#ifdef NEWBORN_SYSTEM_FAMILY_WINDOWS
+  EXPECT_EQ(File::baseName("/foo/bar"), "bar");
+  EXPECT_EQ(File::baseName("\\foo\\bar\\"), "bar");
+  EXPECT_EQ(File::baseName("/foo/bar/baz"), "baz");
+  EXPECT_EQ(File::dirName("\\foo\\bar"), "\\foo");
+  EXPECT_EQ(File::dirName("/foo\\bar/"), "/foo");
+  EXPECT_EQ(File::dirName("/foo/bar\\baz"), "/foo/bar");
+  EXPECT_EQ(File::dirName("foo/bar/baz"), "foo/bar");
+
+  EXPECT_EQ(File::relativeTo("c:\\foo\\", "bar"), "c:\\foo\\bar");
+  EXPECT_EQ(File::relativeTo("c:\\foo", "bar"), "c:\\foo\\bar");
+  EXPECT_EQ(File::relativeTo("c:\\foo\\", "\\bar"), "\\bar");
+  EXPECT_EQ(File::relativeTo("c:\\foo\\", ".\\bar"), "c:\\foo\\bar");
+  EXPECT_EQ(File::relativeTo("c:\\foo\\.", ".\\bar"), "c:\\foo\\bar");
+  EXPECT_EQ(File::relativeTo("c:\\foo\\.", "c:\\bar"), "c:\\bar");
+  EXPECT_EQ(File::relativeTo("c:\\foo\\.", "c:bar\\"), "c:bar\\");
+  EXPECT_EQ(File::relativeTo("c:\\foo.", "bar"), "c:\\foo.\\bar");
+#else
+  EXPECT_EQ(File::baseName("/foo/bar"), "bar");
+  EXPECT_EQ(File::baseName("/foo/bar/"), "bar");
+  EXPECT_EQ(File::baseName("/foo/bar/baz"), "baz");
+  EXPECT_EQ(File::dirName("/foo/bar"), "/foo");
+  EXPECT_EQ(File::dirName("/foo/bar/"), "/foo");
+  EXPECT_EQ(File::dirName("/foo/bar/baz"), "/foo/bar");
+  EXPECT_EQ(File::dirName("foo/bar/baz"), "foo/bar");
+
+  EXPECT_EQ(File::relativeTo("/foo", "bar"), "/foo/bar");
+  EXPECT_EQ(File::relativeTo("/foo", "bar/"), "/foo/bar/");
+  EXPECT_EQ(File::relativeTo("/foo", "/bar/"), "/bar/");
+#endif
+}
