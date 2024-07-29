c242f71a4635fd8a24e0bf38eb1c6616429dabc8
Xarond <joszko.jakub@gmail.com>
2024-07-29 22:42:40 +0200

    Animation and timers

---
+++ source/game/NewbornAnimation.hpp
@@ -0,0 +1,56 @@
+#pragma once
+
+#include "NewbornDrawable.hpp"
+#include "NewbornBiMap.hpp"
+
+namespace Newborn {
+
+NEWBORN_CLASS(Animation);
+
+class Animation {
+public:
+    Animation(Json config = {}, String const& directory = {} );
+
+    void setAngle(float angle);
+
+    void setProcessing(DirectivesGroup processing);
+
+    void setColor(Color color);
+
+    void setTag(String tagName, String tagValue);
+
+    void clearTags();
+
+    Drawable drawable(float pixelSize) const;
+
+    void update(float dt);
+
+    bool isComplete() const;
+    void reset();
+
+private:
+    enum AnimationMode {Stop, EndAndDisappear, LoopForever};
+    static EnumMap<AnimationMode> AnimationModeNames;
+
+    AnimationMode m_mode;
+    String m_directory;
+    String m_base;
+    bool m_appendFrame;
+    int m_frameNumber;
+    float m_animationCycle;
+    float m_animationTime;
+    float m_angle;
+    Vec2F m_offset;
+    bool m_centered;
+    DirectivesGroup m_processing;
+    Color m_color;
+    int m_variantOffset;
+
+    StringMap<String> m_tagValues;
+    int m_frame;
+    float m_animationTimer;
+    float m_timeToLive;
+    bool m_completed;
+};
+
+}
