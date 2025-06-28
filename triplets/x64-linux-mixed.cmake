5bdf0ed2a655a734428163ef3308308c1d31daa8
Xarond <59401139+Xarond@users.noreply.github.com>
2025-06-28 13:02:28 +0200

    nvm

--- triplets/x64-linux-mixed.cmake
+++ triplets/x64-linux-mixed.cmake
@@ -3,14 +3,10 @@ set(VCPKG_CRT_LINKAGE dynamic)
 set(VCPKG_LIBRARY_LINKAGE static)
 
 set(VCPKG_CMAKE_SYSTEM_NAME Linux)
-set(VCPKG_CHAINLOAD_TOOLCHAIN_FILE ${CMAKE_CURRENT_LIST_DIR}/../toolchains/clang-toolchain.cmake)
+set(VCPKG_CHAINLOAD_TOOLCHAIN_FILE ${CMAKE_CURRENT_LIST_DIR}/../toolchains/linux-clang.cmake)
 
 if(PORT MATCHES "discord-")
     set(VCPKG_LIBRARY_LINKAGE dynamic)
-endif()
-if(PORT MATCHES "libsystemd")
-    set(VCPKG_C_FLAGS "-std=c11")
-    set(VCPKG_CXX_FLAGS "-std=c11")
 endif()
 if(PORT MATCHES "opus")
   string(CONCAT VCPKG_CMAKE_CONFIGURE_OPTIONS
