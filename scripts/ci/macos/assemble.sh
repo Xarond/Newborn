03a54f5bb357c9f45e3a0cd75290fb20a8c8ce50
Xarond <59401139+Xarond@users.noreply.github.com>
2025-06-29 19:57:56 +0200

    Use Player:MoveTo instead of mcontroller->setPosition

--- scripts/ci/macos/assemble.sh
+++ scripts/ci/macos/assemble.sh
@@ -13,7 +13,7 @@ mkdir client_distribution/osx/Newborn.app/Contents/MacOS
 cp dist/newborn client_distribution/osx/Newborn.app/Contents/MacOS/
 cp lib/osx/x64/libsteam_api.so/*.dylib client_distribution/osx/Newborn.app/Contents/MacOS/
 cp \
-  dist/starbound_server \
+  dist/newborn_server \
   scripts/ci/macos/nbinit.config \
   scripts/ci/macos/run-server.sh \
   client_distribution/osx/
