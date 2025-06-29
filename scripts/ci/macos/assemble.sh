3d0eb01d4f351f9e96bf104d72534c66f55e6e89
Xarond <59401139+Xarond@users.noreply.github.com>
2025-06-29 19:56:18 +0200

    fix copying osx dylib

--- scripts/ci/macos/assemble.sh
+++ scripts/ci/macos/assemble.sh
@@ -4,7 +4,6 @@ mkdir client_distribution
 mkdir client_distribution/assets
 mkdir client_distribution/assets/user
 
-
 mkdir client_distribution/mods
 touch client_distribution/mods/mods_go_here
 
@@ -12,10 +11,11 @@ mkdir client_distribution/osx
 cp -LR scripts/ci/macos/Newborn.app client_distribution/osx/
 mkdir client_distribution/osx/Newborn.app/Contents/MacOS
 cp dist/newborn client_distribution/osx/Newborn.app/Contents/MacOS/
-cp dist/*.dylib client_distribution/osx/Newborn.app/Contents/MacOS/
+cp lib/osx/x64/libsteam_api.so/*.dylib client_distribution/osx/Newborn.app/Contents/MacOS/
 cp \
-  dist/newborn_server \
-  dist/planet_mapgen \
+  dist/starbound_server \
   scripts/ci/macos/nbinit.config \
   scripts/ci/macos/run-server.sh \
   client_distribution/osx/
+
+tar -cvf client.tar client_distribution
